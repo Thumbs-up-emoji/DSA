2946677845db307d847177f36043317ef64c7725
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2024-02-08 23:50:10 +0530

    DSA_Vector

---
+++ Acads/L3_Q2_Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "DSA_Vector.cpp"
+#include "L3_Q2_YourBITSid.cpp"
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t > 0)
+    {
+        t--;
+        DSA_Vector v;
+
+        solve(v); //Find the index of the largest and the smallest element
+    }
+    return 0;
+}
---
+++ Acads/L3_Q2_YourBITSid.cpp
@@ -0,0 +1,11 @@
+void solve(DSA_Vector& v){
+    // *************************************************
+    // WRITE YOUR CODE HERE
+
+
+
+    //The indices printed below are wrong
+    cout<<0<<" "<<v.Size()<<" "<<endl;
+
+    // *************************************************
+}
