492216d9e0122db0861a3d2cdd3cbc31f909977b
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2024-03-01 07:04:43 +0530

    practice problems

---
+++ 1353.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+static bool cmp(vector<int>& a,vector<int>& b)
+{
+    if(a[1]<b[1])return true;
+    else if(a[1]==b[1]) return a[0]<b[0];
+    return false;
+}
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(),events.end(),cmp);
+        set<int>days;
+        int cnt=0;
+        for(int i= 1 ;i<=100000;++i)
+        {
+            days.insert(i);
+        }
+        for(const auto & event : events)
+        {
+            int s = event[0];
+            int e = event[1];
+            auto it = days.lower_bound(s);
+            if(it == days.end() || *it >e)
+            {
+                continue;
+            }
+            else
+            {
+                ++cnt;
+                days.erase(it);
+            }
+        }
+        return cnt;
+    }
+};
