adc903e05cd264adbd2b52d61bab8f0980386f1f
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2024-03-05 17:50:02 +0530

    sliding window

---
+++ 3.cpp
@@ -0,0 +1,15 @@
+//not mine (some may not be marked as not mine)
+#include<bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
